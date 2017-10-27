338bf7264b98756301b86087f51ad66f358cf431
Hiroki Kawaguchi <hiroki.kwgch@gmail.com>
2017-10-27 21:32:58 +0900

    assert_equalを複数行に対応

--- test.sh
+++ test.sh
@@ -14,10 +14,16 @@ assert_equal() {
     local expected=${1}
     local actual=${2}
 
-    if [ ${expected} == ${actual} ]; then
+    diff <(echo -e "${expected}") <(echo -e "${actual}") > /dev/null
+    local exit_code=$?
+    
+    if [ "${exit_code}" == "0" ]; then
         ok "equal. expected=\"${expected}\", actual=\"${actual}\""
     else
         ng "NOT equal. expected=\"${expected}\", actual=\"${actual}\""
+        echo -e "\ndiff: "
+        diff <(echo -e "${expected}") <(echo -e "${actual}")
+        echo ""
     fi 
 }
 
@@ -28,4 +34,39 @@ assert_equal "orange" "orange"
 # 目視確認 -> OK: equal. expected="orange", actual="orange"
 
 assert_equal "orange" "mikan"
-# 目視確認 -> NG: NOT equal. expected="apple", actual="apple"
+# 目視確認 -> NG: NOT equal. expected="orange", actual="mikan"
+
+expected=`cat <<EOF
+a
+b
+c
+EOF
+` 
+actual=`cat <<EOF
+fizz
+buzz
+EOF
+`
+assert_equal "${expected}" "${actual}"
+# 目視確認
+# NG: NOT equal. expected="a
+# b
+# c", actual="fizz
+# buzz"
+#
+# diff:
+# 1,3c1,2
+# < a
+# < b
+# < c
+# ---
+# > fizz
+# > buzz
+
+
+# テストケース
+test_main() {
+    local actual=`echo -e "menu,price\npie,100" | mdt`
+
+}
+
